c9be18a0c80c150a72f372ec271adba1ed2b7144
windspring <sagan.pawel1000@gmail.com>
2020-04-14 09:49:43 +0200

    instead of jsp, I configured thymeleaf, cause it was much simpler

--- src/main/java/snakeApp.java
+++ src/main/java/snakeApp.java
@@ -2,11 +2,16 @@ package rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.servlet.http.*;
 import java.net.URI;
+import javax.ws.rs.core.MediaType;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 @Path("/app")
 public class snakeApp{
@@ -14,6 +19,9 @@ public class snakeApp{
     @Context
     private HttpServletRequest request;
     
+	@Context
+	private HttpServletResponse response;
+
     @GET
     public Response renderApp()
     {
@@ -24,7 +32,6 @@ public class snakeApp{
         }
 
         userDescriptor u = (userDescriptor)session.getAttribute("user_info");
-        
         return Response.ok("User authenticated, privilege: " + u.getprivilege()).build();
     }
 
@@ -41,4 +48,22 @@ public class snakeApp{
         URI uri = new URI("rest/logout");
         return Response.seeOther(uri).build();
     }
+
+    @GET
+    @Path("/test")
+	@Produces({ MediaType.TEXT_HTML })
+    public String getMain() 
+    {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(request.getServletContext());
+		templateResolver.setTemplateMode("HTML5");
+
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+
+		WebContext context = new WebContext(request, response, request.getServletContext());
+
+		context.setVariable("test", "This is a string that will be mapped to the variable 'test'");
+
+		return templateEngine.process("/test.html", context);
+	}
 };
